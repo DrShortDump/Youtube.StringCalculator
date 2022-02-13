2c2ae8fd8f58c2202ae74986374646e8e0f0a37f
Ramy El-Arnaouty <ceysersoze@gmail.com>
2022-02-14 00:47:33 +0100

    refactoring 2

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace StringCalculator
 {
@@ -20,28 +19,22 @@ namespace StringCalculator
                 // split the delimiter defnition(s) and the numbers.
                 var splitOnFirstNewLine = numbers.Split(new[] { '\n' }, 2);
 
-                // check for single custom delimiter like ';'
+                // check for single custom delimiters like ';'
                 try
-                {
-                    var customDelimiter = splitOnFirstNewLine[0].Replace("//", string.Empty).Single();
-                    delimiters.Add($"{customDelimiter}");
+                {                  
+                    delimiters.Add(splitOnFirstNewLine[0].Replace("//", string.Empty).Single().ToString());
                 }
-                catch (Exception ex)
+                catch (InvalidOperationException)
                 {
-                    if (ex is InvalidOperationException)
-                    {
-                        // Delimiter sequence contains more than one element.
-                        StringBuilder sb = new StringBuilder(splitOnFirstNewLine[0]);
-                        sb.Replace("//", string.Empty);
+                    // Delimiter sequence contains more than one element.
+                    StringBuilder delimiterSequence = new StringBuilder(splitOnFirstNewLine[0]).Replace("//", string.Empty);
 
-                        // check for custom delimiters of any length enclosed by "[]" e.g. "[*][%%][---]".
-                        Regex reg = new Regex($"{Regex.Escape("[")}[^{Regex.Escape("]")}]+{Regex.Escape("]")}");
-                        foreach (Match m in reg.Matches(sb.ToString()))
-                        {
-                            // Extract delimiters.
-                            delimiters.Add(m.Value.Substring(1, m.Value.Length - 2));
-                        }
-                    }
+                    // check for custom delimiters of any length enclosed by "[]" e.g. "[*][%%][---]".
+                    Regex regexForCustomDelimiters = new Regex($"{Regex.Escape("[")}[^{Regex.Escape("]")}]+{Regex.Escape("]")}");
+                    
+                    // Extract delimiters.
+                    delimiters.AddRange(from Match customDelimiter in regexForCustomDelimiters.Matches(delimiterSequence.ToString())
+                                        select customDelimiter.Value.Substring(1, customDelimiter.Value.Length - 2));
                 }
 
                 // split the number string at all known delimiters.
